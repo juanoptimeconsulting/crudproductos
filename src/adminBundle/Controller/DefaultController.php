9668aefd21bb39794602bd7a960752bb4254baad
juanoptime <jdiaz@optimeconsulting.com>
2018-12-18 08:48:14 -0500

    proof of listing

--- src/adminBundle/Controller/DefaultController.php
+++ src/adminBundle/Controller/DefaultController.php
@@ -8,6 +8,17 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $product_repo = $em->getRepository("adminBundle:Product");
+        $products = $product_repo->findAll();
+        foreach ($products as $run){
+            echo  $run->getName()."</br>";
+            echo  $run->getCategory()->getName()."</br>";
+        }
+
+        die();
+
         return $this->render('adminBundle:Default:index.html.twig');
     }
 }
